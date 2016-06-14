f30406fe1b6efd529991bb120aca72ad0e8ecb0e
Guocheng Zheng <121431418@qq.com>
2016-06-14 18:04:29 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if ! -f $CATALINA_HOME/webapps/ROOT.war && -f $CODE/pom.xml; then
+if [! -f $CATALINA_HOME/webapps/ROOT.war] && [-f $CODE/pom.xml]; then
 	cd $CODE
 	mvn install
 	rm -rf $CATALINA_HOME/webapps/* 
 	cp target/*.war $CATALINA_HOME/webapps/ROOT.war
 	rm -rf /usr/bin/mvn
 	rm -rf $MAVEN_HOME
-	if ! $DELETE_M2 = "false" || $DELETE_M2 = "true" || $DELETE_M2 = "yes" || ! $DELETE_M2 = "no"; then
+	if [! $DELETE_M2 = "false"] || [$DELETE_M2 = "true"] || [$DELETE_M2 = "yes"] || [! $DELETE_M2 = "no"]; then
 		rm -rf $DELETE_M2;
 	fi
 fi
